0829131a895f6e13a78a38d02b494dedd6130135
Christian Martins <martins.christian@hotmail.com>
2025-09-26 16:24:13 -0300

    test: added unit tests

---
+++ tests/NBrzId.Validator.DI.UnitTests/Mocks/ExternalIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using NBrzId.Common;
+
+namespace NBrzId.Validator.DI.UnitTests.Mocks
+{
+    internal sealed class ExternalIdentifier : IBrzIdentifier
+    {
+        public char[] FormattingCharacters => new char[] { '.' };
+
+        public char PaddingCharacter => '0';
+
+        public int Length => 5;
+
+        public string Mask => "NNNN/N";
+    }
+}
---
+++ tests/NBrzId.Validator.DI.UnitTests/Mocks/ExternalIdentifierValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace NBrzId.Validator.DI.UnitTests.Mocks
+{
+    internal sealed class ExternalIdentifierValidator : IBrzIdentifierValidator<ExternalIdentifier>
+    {
+        public bool ApplyValidation(string value, string auxiliaryValue = null, bool removeFormatters = true, bool pad = false) => true;
+    }
+}
